b95a3347a344db038d36e9e6e41038d3cad91ec4
bahing-rai <mahchaibahing@gmail.com>
2024-05-18 17:40:07 +0545

    changed

--- sales.py
+++ sales.py
@@ -10,7 +10,8 @@ def sales():
         exit
 
     if a=='Yes':
-        b=eval(input('Select what u want to buy:'))
+        b=eval(input('Select what u want to buy: 1.Apple 2.Banana 3.Rice 4.Milk 5.Bread'))
+        c=eval(input('Enter quantity of the product:'))
 
     print('Please do visit again.')
     
