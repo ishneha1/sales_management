7d3782844370aef2d98c4cdef8056c09a097f829
unknown <thakaliishneha_31@icloud.com>
2024-05-18 17:15:20 +0545

    sales and stocks

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # sales_management
 This application check the sales and product of a shop
+
+This is a collaborative programme.
+Developers:
+-Ishneha Hirachan
+-Simon Rai
---
+++ stock.py
@@ -0,0 +1,11 @@
+
+def stock():
+    in_stock={
+    "grocerries" : ["apple","banana","rice","milk","bread"],
+    "toiletries" : ['tissue','toothbrush','toothpaste'],
+    "stationery" : ['pen','copy'],
+    "clothes"    : ['tshirt','pant','jacket']
+}
+
+stock()
+
